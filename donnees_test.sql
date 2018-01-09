3d4b684fb661f3de080fb193e0fc9db6333cb870
Nanro <ronan_lh@hotmail.fr>
2018-01-09 21:38:48 +0100

    Updated donnees_test.sql

--- donnees_test.sql
+++ donnees_test.sql
@@ -24,6 +24,31 @@ VALUES ('MARGARITA', 1, 3),
 ('REINE', 4, 7),
 ('REINE', 5,1);
 
+INSERT INTO Etablissement (nom, adresse)
+VALUES ('GO COWS', 'South park, Colorado'),
+('BRONCOS', 'Denver, Colorado')
+
+
+INSERT INTO Employe (nom, prenom)
+VALUES ('MARSH', 'Stan'),
+('BROFLOVSKI', 'Kyle'),
+('CARTMAN', 'Eric'),
+('McCORMICK', 'Kenny');
+
+INSERT INTO Preparateur (employe_id, etablissment_id)
+VALUES (1,1),
+(2,1)
+
+INSERT INTO Livreur (employe_id, immatriculation_scooter)
+VALUES (3, '666GAGA')
+
+INSERT INTO Vendeur (employe_id, etablissment_id)
+VALUES (4,1)
+
+INSERT INTO Client (nom, prenom, adresse, email, motdepasse)
+VALUES ('MARSH', 'Randy', '101 Main street, South Park, Colorado', 'randymarch@southpark.com', 'beer'),
+('KERN', 'Jimbo', '112 Main street, South Park, COlorado', 'jimbokern@southpark.com', 'comingrightforus')
+
 
 SELECT Ingredients.nom as nom_ingredient, Recette.quantite*Ingredients.poids FROM Recette
 INNER JOIN Pizza ON Pizza.nom=Recette.nom_pizza
